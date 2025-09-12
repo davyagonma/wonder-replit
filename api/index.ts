1f249431c31b3f5dcb90251f6b443f1559876ce3
davyagonma <singbodavyagonma@gmail.com>
2025-09-13 00:55:28 +0100

    vercel add

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
 // 👉 importe tes routes ici
 
