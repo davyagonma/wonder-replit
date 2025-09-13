eac298d7f80e8b03901e393fbe1d24d8b5891390
davyagonma <singbodavyagonma@gmail.com>
2025-09-13 02:11:58 +0100

    vercel add

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
+
 // export default defineConfig({
 //   plugins: [react()],
 //   base: "./", // Important pour Vercel
