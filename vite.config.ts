a78bb7a5744463191a85afaabb54cdcf986c928e
davyagonma <singbodavyagonma@gmail.com>
2025-09-13 01:58:14 +0100

    vercel add

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "./", // Important pour Vercel
+//   build: {
+//     outDir: "dist" // dossier de sortie
+//   }
+// });
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
